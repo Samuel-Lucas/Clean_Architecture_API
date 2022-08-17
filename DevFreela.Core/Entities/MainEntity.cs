9339ca52b2c9de70ad741e9e6279c6ea0b116a34
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-08-16 22:33:13 -0300

    Criando projeto, definindo entidades no core

---
+++ DevFreela.Core/Entities/MainEntity.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public abstract class MainEntity
+    {
+        public int Id { get; private set; }
+    }
+}
---
+++ DevFreela.Core/Entities/Project.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.Core.Entities
+{
+    public class Project : MainEntity
+    {
+        public Project(string title, string description, int idClient, int idFreelancer, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            IdClient = idClient;
+            IdFreelancer = idFreelancer;
+            TotalCost = totalCost;
+
+            CreatedAt = DateTime.Now;
+            Status = ProjectStatusEnum.Created;
+            Comments = new List<ProjectComment>();
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int IdClient { get; private set; }
+        public int IdFreelancer { get; private set; }
+        public decimal TotalCost { get; set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public ProjectStatusEnum Status { get; set; }
+        public List<ProjectComment> Comments { get; private set; }
+    }
+}
